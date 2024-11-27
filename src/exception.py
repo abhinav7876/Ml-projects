45ee5b426acc2beb379db5db5e66e636e8d4754b
abhinav7876 <singhalabhinav31@gmail.com>
2024-11-27 16:42:47 +0530

    exception and logger

---
+++ src/exception.py
@@ -0,0 +1,15 @@
+import sys
+import logging
+import logger
+
+class Custom_exception(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=get_error_message(error_message,error_detail=error_detail)
+    def __str__(self):
+        return self.error_message
+def get_error_message(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message=" Error occured in python filename [{0}] line number [{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+    return error_message
