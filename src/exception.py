e3329e3e06afc99d7bdf2f7bdff810416c8f943c
John Doe <singhalabhinav31@gmail.com>
2025-04-07 01:05:54 +0530

    changed requirements.txt

--- src/exception.py
+++ src/exception.py
@@ -3,9 +3,9 @@ from src.logger import logging
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
+        super().__init__(error_message)  #inhertance from exception class
         self.error_message=get_error_message(error_message,error_detail=error_detail)
-    def __str__(self):
+    def __str__(self):    #when we print then this error message will return
         return self.error_message
 def get_error_message(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
